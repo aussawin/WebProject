b094bd64a6ffd1523ac66852a6a4587f8c5c5cff
aussawin.khaokum <aussawin.khaokum@accenture.com>
2018-11-03 16:42:49 +0700

    Added navbar component

---
+++ src/components/NavBar.vue
@@ -0,0 +1,28 @@
+<template>
+    <nav class="navbar bg-dark fixed-top navbar-light bg-light">
+        <div class="container">
+            <a class="navbar-brand">
+                <img src="../assets/Image/1_Home/logo01.png" width="100" height="100" class="d-inline-block align-top" alt="WindSor">
+            </a>
+            <ul class="nav navbar-nav mx-auto text-light">Home</ul>
+            <ul class="nav navbar-nav mx-auto text-light">Company</ul>
+            <ul class="nav navbar-nav mx-auto text-light">Video</ul>
+            <ul class="nav navbar-nav mx-auto text-light">Product</ul>
+            <ul class="nav navbar-nav mx-auto text-light">Society</ul>
+            <ul class="nav navbar-nav mx-auto text-light">Award</ul>
+            <ul class="nav navbar-nav mx-auto text-light">Contact us</ul>
+        </div>
+    </nav>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+    .bg-dark {
+        background-color: transparent !important
+    }
+</style>
