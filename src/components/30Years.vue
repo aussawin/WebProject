71bcc72e06784ee7db76566f020f0bffd673a8b2
aussawin.khaokum <aussawin.khaokum@accenture.com>
2018-11-03 21:20:20 +0700

    Update the fucking 30 year

---
+++ src/components/30Years.vue
@@ -0,0 +1,60 @@
+<template>
+    <div id = "thirty-years">
+        <div class="grid-container">
+            <div class="area1">
+                <div class="wrapper">
+                    <div class="item1">
+                        <img src="../assets/Image/2_30years/logo-30.png">
+                    </div>
+                </div>
+            </div>
+            <div class="area2 ">
+                <center><h1><b>OF CONSTANT</b></h1></center>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>    
+export default {
+
+}
+</script>
+
+<style>
+#thirty-years {
+    width: 100vw;
+    height: 80vh;
+    background-image: url("../assets/Image/2_30years/BG.png");
+    background-size: cover;
+    background-repeat: no-repeat;
+}
+.grid-container {
+  display: grid;
+  grid-template-areas: 'area1 area2';
+  height: 100%;
+  width: 80%;
+}
+.area1 { grid-area: area1; }
+.area2 { grid-area: area2; }
+.center {
+    margin: auto;
+}
+
+.wrapper {
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  grid-auto-rows: 20vh;
+  grid-template-areas: 
+    ". . . ."
+    ". a a ."
+    ". a a ."
+    ". . . .";
+}
+.item1 {
+  grid-area: a;
+  align-self: center;
+  justify-self: center;
+}
+
+</style>
--- src/components/Main.vue
+++ src/components/Main.vue
@@ -2,18 +2,21 @@
     <div id="main">
         <navbar></navbar>
         <carousel></carousel>
+        <thirty-years></thirty-years>
     </div>
 </template>
 
 <script>
 import NavBar from './NavBar.vue';
 import Carousel from "./Carousel.vue"
+import ThirtyYears from './30Years.vue'
 
 export default {
     name: "Main",
   components: {
     "carousel": Carousel,
-    "navbar" : NavBar
+    "navbar" : NavBar,
+    "thirty-years" : ThirtyYears
   }
 }
 </script>
