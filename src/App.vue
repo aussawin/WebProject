6033b192c7ca77a8ede4176a0ccaa4717513a7e8
Surarerk Boonjit <surarerk.boonjit@gmail.com>
2018-11-03 15:03:10 +0700

    change to vue.js

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app">
+    <router-view/>
+  </div>
+</template>
+
+<script>
+import Carousel from "./components/Carousel"
+
+export default {
+  name: 'App',
+  components: {
+    Carousel: "carousel"
+  }
+}
+</script>
+
+<style>
+#app {
+  
+}
+</style>
