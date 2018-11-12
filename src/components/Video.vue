daf8d36cd537e0904ba662286ff4d978da569e39
aussawin.khaokum <aussawin.khaokum@accenture.com>
2018-11-13 06:46:25 +0700

    Doce video page

--- src/components/Video.vue
+++ src/components/Video.vue
@@ -1,9 +1,9 @@
 <template>
   <div id="video-page">
-    <img src="../assets/Image/4_VIDEO/Head.png"><br><br><br>
-    <center>
-      <iframe width="720" height="360" src="https://www.youtube.com/watch?time_continue=182&v=QlhqGoDDF5c"></iframe>
-    </center>
+    <img src="../assets/Image/4_VIDEO/Head.png"><br><br>
+    <left>
+      <iframe class="full-size-video" src="https://www.youtube.com/embed/QlhqGoDDF5c" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    </left>
   </div>
 </template>
 
@@ -12,11 +12,15 @@
  
   #video-page {
     width: 100vw;
-    height: 70vh;
+    height: 80vh;
     padding-top: 5vh;
     padding-bottom: 10vh;
     padding-left: 15vw;
     padding-right: 15vw;
     background-color: white;
+  }
+  .full-size-video {
+    width: 70vw;
+    height: 60vh;
   }
 </style>
